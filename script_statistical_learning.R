9f3dcad1cde0d7fd0abff7b63d3ea5c14055bfe4
Valerio <valeriorocca999@gmail.com>
2023-03-30 13:21:25 +0200

    modifica no_biofuel

--- script_statistical_learning.R
+++ script_statistical_learning.R
@@ -1,6 +1,8 @@
 # Libraries
 library(dplyr)
 library(tidyr)
+library(ggplot2)
+library(viridis)
 
 # Remove variables from main dataframe
 main = select(total_energy_data, -c("gdp", "biofuel_cons_change_pct",
@@ -23,12 +25,12 @@ main = select(total_energy_data, -c("gdp", "biofuel_cons_change_pct",
             "nuclear_elec_per_capita", "nuclear_energy_per_capita",
             "oil_prod_per_capita", "gas_energy_per_capita",
             "oil_elec_per_capita", "oil_prod_change_pct",
-            "oil_prod_change_twh", "other_renewable_electricity",
+            "oil_prod_change_twh", "other_renewable_exc_biofuel_electricity",
             "other_renewables_cons_change_pct",
             "other_renewables_cons_change_twh",
             "other_renewables_elec_per_capita",
             "other_renewables_elec_per_capita_exc_biofuel",
-            "other_renewables_energy_per_capita", "other_renewables_share_elec",
+            "other_renewables_energy_per_capita", "other_renewables_share_elec_exc_biofuel",
             "per_capita_electricity","renewables_cons_change_pct",
             "renewables_cons_change_twh", "renewables_elec_per_capita",
             "renewables_energy_per_capita", "solar_cons_change_pct",
@@ -36,18 +38,20 @@ main = select(total_energy_data, -c("gdp", "biofuel_cons_change_pct",
             "wind_cons_change_pct", "wind_cons_change_twh",
             "solar_energy_per_capita", "wind_elec_per_capita",
             "wind_energy_per_capita", "oil_cons_change_pct",
-            "oil_cons_change_twh", "oil_energy_per_capita"))
+            "oil_cons_change_twh", "oil_energy_per_capita",
+            "biofuel_consumption", "biofuel_electricity",
+            "biofuel_share_elec","biofuel_share_energy"))
 
 # Separate biofuel consumption from other renewables; rename the variables
 
-main = mutate(main,
-              other_renewable_consumption = other_renewable_consumption - biofuel_consumption,
-              other_renewable_electricity = other_renewable_exc_biofuel_electricity,
-              other_renewables_share_elec = other_renewables_share_elec_exc_biofuel,
-              other_renewables_share_energy = other_renewables_share_energy - biofuel_share_energy)
+#main = mutate(main,
+#              other_renewable_consumption = other_renewable_consumption - biofuel_consumption,
+#              other_renewable_electricity = other_renewable_exc_biofuel_electricity,
+#              other_renewables_share_elec = other_renewables_share_elec_exc_biofuel,
+#              other_renewables_share_energy = other_renewables_share_energy - biofuel_share_energy)
  
-main = select(main, -c("other_renewable_exc_biofuel_electricity",
-                                    "other_renewables_share_elec_exc_biofuel"))
+#main = select(main, -c("other_renewable_exc_biofuel_electricity",
+#                                    "other_renewables_share_elec_exc_biofuel"))
 
 # Remove variables without an ISO code
 
@@ -150,7 +154,7 @@ rm(gdp)
 rm(total_energy_data)
 
 # Change variables names
-colnames(main) = c(colnames(main[,1:63]), "land_area", "hdi", "urbaniz_rate",
+colnames(main) = c(colnames(main[,1:59]), "land_area", "hdi", "urbaniz_rate",
                            "particulate_pollution", "agri_land_rate",
                            "coal_reserves_2021", "oil_reserves_2012",
                            "uranium_reserves_2019","gas_reserves",
@@ -159,10 +163,138 @@ colnames(main) = c(colnames(main[,1:63]), "land_area", "hdi", "urbaniz_rate",
 # Change ".." to NA
 main = mutate(main, government_expenditure = na_if(government_expenditure, ".."),
                gdp = na_if(gdp, ".."))
-main$government_expenditure = as.numeric(prova$government_expenditure)
-main$gdp = as.numeric(prova$gdp)
+main$government_expenditure = as.numeric(main$government_expenditure)
+main$gdp = as.numeric(main$gdp)
+
+
 
 
 # ANALISI DESCRITTIVA DEL DATASET
 
-tot_renew = ggplot(main, aes(hwy))
+# Andamento nel consumo delle rinnovabili nel mondo
+# ... diviso per fonte
+# ... diviso per geografia economica e/o territoriale
+# Focus su qualche outlier e/o Italia
+
+# Dati: electricity consumption + share electricity +
+#       consumo energia primaria + share energia primaria
+
+
+# Total and share in the world from renewables and low carbon
+# ------
+
+# Total renewable electricity
+total_ren_elec = main[c("year","renewables_electricity")] %>%
+  mutate_all(~replace_na(.,0)) %>%
+  group_by(year) %>%
+  summarize(world_renew_elect = sum(renewables_electricity))
+
+# Share renewable electricity
+share_ren_elec = main[c("year","renewables_share_elec")] %>%
+  mutate_all(~replace_na(.,0)) %>%
+  group_by(year) %>%
+  summarize(world_renew_elect_share = sum(renewables_share_elec))
+# ----
+
+# Total and share of low carbon electricity by type
+# ----
+
+# Total low carbon electricity generation in the world grouped by type
+total_lc_elec_grouped = main[c("year", "hydro_electricity",
+    "nuclear_electricity", "solar_electricity", "wind_electricity",
+    "other_renewable_electricity")] %>%
+  mutate_all(~replace_na(.,0)) %>%
+  group_by(year) %>%
+  summarize(world_nuclear_elec = sum(nuclear_electricity),
+            world_hydro_elec = sum(hydro_electricity),
+            world_wind_elec = sum(wind_electricity),
+            world_solar_elec = sum(solar_electricity),
+            world_other_elec = sum(other_renewable_electricity),
+            world_nuclear_elec = sum(nuclear_electricity)) %>%
+  gather(key = "type",
+         value = "world_elect",
+         -year)
+total_lc_elec_grouped$type = factor(total_lc_elec_grouped$type,
+  levels = c("world_nuclear_elec", "world_hydro_elec", "world_solar_elec", 
+    "world_wind_elec", "world_other_elec"))
+
+# Share of low carbon electricity generation in the world grouped by type
+share_lc_elec_grouped = main[c("year","biofuel_electricity", "hydro_electricity",
+                               "nuclear_electricity", "solar_electricity", "wind_electricity",
+                               "other_renewable_electricity")] %>%
+  mutate_all(~replace_na(.,0)) %>%
+  group_by(year) %>%
+  summarize(world_nuclear_elec = sum(nuclear_electricity),
+            world_hydro_elec = sum(hydro_electricity),
+            world_wind_elec = sum(wind_electricity),
+            world_solar_elec = sum(solar_electricity),
+            world_biofuel_elec = sum(biofuel_electricity),
+            world_other_elec = sum(other_renewable_electricity)) %>%
+  select(world_nuclear_elec, world_hydro_elec, world_wind_elec, world_solar_elec,
+         world_biofuel_elec, world_other_elec, world_nuclear_elec) %>%
+  mutate(year = 1900:2022, row_total = rowSums(.)) %>%
+  mutate(across(world_nuclear_elec:world_other_elec, ~ . / row_total * 100)) %>%
+  select(-row_total) %>%
+  gather(key = "type",
+         value = "world_elect",
+         -year)
+share_lc_elec_grouped$type = factor(share_lc_elec_grouped$type,
+      levels = c("world_nuclear_elec", "world_hydro_elec", "world_solar_elec", 
+                 "world_wind_elec", "world_biofuel_elec", "world_other_elec"))
+  
+
+# Graphs
+#----
+# Total world electricity production from renewables
+ggplot(total_ren_elec, aes(year, world_renew_elect)) +
+  geom_area()+
+  scale_x_continuous(limits = c(1975,2020))
+
+# Total world electricity production from low carbon, grouped by type
+ggplot(total_lc_elec_grouped, aes(year, world_elect, fill = type)) +
+  geom_area(alpha=0.6 , size=.5, colour="white")+
+  scale_x_continuous(limits = c(1975,2020)) +
+  scale_fill_viridis(discrete = T)
+
+# Share of world electricity production from low carbon, grouped by type
+ggplot(share_lc_elec_grouped, aes(year, world_elect, fill = type)) +
+  geom_area(alpha=0.6 , size=.5, colour="white")+
+  scale_x_continuous(limits = c(1975,2020)) +
+  scale_fill_viridis(discrete = T)
+
+
+
+
+
+
+
+
+
+# Share of world electricity production from renewables
+prova = main[c("year","electricity_generation","renewables_share_elec")] %>%
+  mutate_all(~replace_na(.,0)) %>%
+  mutate(country = main$iso_code) %>%
+  group_by(year) %>%
+  mutate(elect_rate = electricity_generation/sum(electricity_generation)) %>%
+  summarise(renew_share_elec = sum(renewables_share_elec * elect_rate))
+
+ggplot(prova, aes(year, renew_share_elec)) +
+  geom_line() +
+  scale_x_continuous(limits = c(1985,2020)) +
+  scale_y_continuous(limits = c(15, 30))
+  
+
+
+# Total electricity production in the world from low carbon
+prova_2 = main[c("year","low_carbon_electricity")] %>%
+  mutate_all(~replace_na(.,0)) %>%
+  group_by(year) %>%
+  summarize(world_lc_elect = sum(low_carbon_electricity))
+
+ggplot(prova_2, aes(year, world_lc_elect,)) +
+  geom_line(col = "red") +
+  geom_line(aes(year, prova$world_renew_elect), col = "blue") +
+  scale_x_continuous(limits = c(1975,2020))
+
+
+
